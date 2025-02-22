599ac323d584deb0e062177d875f8fefcbc96d17
PilarFernandezNieto <apreilmc@gmail.com>
2025-02-22 22:52:40 +0100

    Email Notification

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $frontendUrl = config('app.frontend_url');
+        $url = url($frontendUrl.'/password-reset/'.$this->token.'?email='.$notifiable->email);
+        return (new MailMessage)
+        ->subject('Restablecer contraseña')
+        ->line('Has solicitado restablecer tu contraseña.')
+        ->action('Restablecer Contraseña', $url)
+        ->line('Si no has solicitado este cambio, ignora este mensaje.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
