29331fc49a9b85066f874147aa9b473b2c3ec1a4
PilarFernandezNieto <apreilmc@gmail.com>
2025-02-22 20:37:13 +0100

    carpeta lang

---
+++ lang/es/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Lenguaje de Paginación
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de lenguaje son utilizadas por la librería de paginación
+    | para construir los enlaces de paginación simples. Puedes cambiarlas por cualquier
+    | cosa que desees para personalizar tus vistas y que coincidan mejor con tu aplicación.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Siguiente &raquo;',
+
+];
---
+++ lang/es/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de Lenguaje de Restablecimiento de Contraseña
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de lenguaje son las predeterminadas que coinciden
+    | con las razones dadas por el broker de contraseñas para un intento de
+    | actualización de contraseña, como el fallo debido a un token de restablecimiento
+    | o una contraseña inválida.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Te hemos enviado un correo electrónico con tu enlace de recuperación.',
+    'throttled' => 'Por favor, espera antes de intentar nuevamente.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
+
+];
