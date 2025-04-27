2ee72a1f07320fa62f094dd48d48f670c8985ef7
PilarFernandezNieto <apreilmc@gmail.com>
2025-04-27 12:13:28 +0200

    CRUD Categorias

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CategoriaRequest;
+use App\Http\Resources\CategoriaCollection;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new CategoriaCollection(Categoria::orderBy('nombre', 'ASC')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoriaRequest $request)
+    {
+        $datos = $request->validated();
+
+        $categoria = Categoria::create([
+            'nombre' => $datos['nombre']
+        ]);
+
+        return [
+            "type" => "success",
+            "categoria" => $categoria,
+            "message" => "Categoría creada correctamente"
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categoria $categoria)
+    {
+        return $categoria;
+    }
+
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoriaRequest $request, Categoria $categoria)
+    {
+
+        $datos = $request->validated();
+
+        $categoria->update([
+            'nombre' => $datos['nombre']
+        ]);
+
+        return [
+            "type" => "success",
+            "categoria" => $categoria,
+            "message" => "Categoría actualizada correctamente"
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categoria $categoria )
+    {
+        $categoria->delete();
+
+        return [
+            "type" => "success",
+            "message" => "Categoría eliminada correctamente"
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\RecetaController;
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\DificultadController;
 use App\Http\Controllers\IngredienteController;
-use App\Http\Controllers\MainController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -14,6 +15,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::prefix('admin')->middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::apiResource('/ingredientes', IngredienteController::class);
+    Route::apiResource('/categorias', CategoriaController::class);
 
     Route::get('/ingredientes-todos', [IngredienteController::class, 'ingredientesTodos']);
 
