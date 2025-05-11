4dcb95941e172a7f1a7f6e7d24fb02f912b2924b
PilarFernandezNieto <apreilmc@gmail.com>
2025-05-11 19:49:41 +0200

    cambios para cors

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://recetas-backend-main-q01e2u.laravel.cloud'),
 
     /*
     |--------------------------------------------------------------------------
