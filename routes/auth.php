7c8a69f2e360ac5038d57a36044189ebe38749ca
PilarFernandezNieto <apreilmc@gmail.com>
2025-07-19 22:58:57 +0200

    deploy success

--- routes/auth.php
+++ routes/auth.php
@@ -8,13 +8,6 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/check-mail', function () {
-    return [
-        'env' => env('MAIL_HOST'),
-        'config' => config('mail.mailers.smtp.host'),
-    ];
-});
-
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
