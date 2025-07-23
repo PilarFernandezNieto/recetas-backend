9c946df5e0e1341ec5eee68a4bae01881093e970
PilarFernandezNieto <apreilmc@gmail.com>
2025-07-23 20:44:50 +0200

    Curd usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
@@ -28,26 +30,43 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-public function show(User $user)
-{
-    return new UserResource($user);
-}
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
 
 
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UserRequest $request, User  $user)
     {
-        //
+        $datos = $request->validated();
+
+        $user->update([
+            'name' => $datos['name'],
+            'email' => $datos['email'],
+            'password' => bcrypt($datos['password']),
+            'is_admin' => $datos['is_admin'] ?? false,
+        ]);
+        return [
+            "type" => "success",
+            "user" => $user,
+            "message" => "Usuario actualizado correctamente"
+        ];
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return [
+            "type" => "success",
+            "message" => "Usuario eliminado correctamente"
+        ];
     }
 }
